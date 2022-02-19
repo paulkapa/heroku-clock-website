f8176df9b27a1439f35562f73325045f1c2ea265
Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>
2022-02-19 12:37:54 +0100

    Initial commit
    Signed-off-by: Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}\\views\\index.html`);
+});
+
+app.listen(PORT, console.log(`Listening on port ${PORT}...`));
