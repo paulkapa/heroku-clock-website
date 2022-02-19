34549dbf007ab34fa40336b8e6642b40224d7021
Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>
2022-02-19 21:22:05 +0100

    Added resources
    Signed-off-by: Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(`${__dirname}/views`));
+app.set(`views`, `${__dirname}/views`);
+
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.sendFile(`index.html`);
+    console.log("GET /");
 });
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
