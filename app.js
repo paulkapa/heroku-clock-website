b7a783c54d67ebc909f1992d10b46d2c14156ba6
Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>
2022-02-19 12:44:57 +0100

    Changed to relative pathing
    Signed-off-by: Paul Capatina-Voila <33447471+paulkapa@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}\\views\\index.html`);
+    res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
